1e7f454dea5f6f8c40a8dd07366325a78b4952bf
nowf41 <wheat.komugi41@gmail.com>
2023-10-19 01:11:11 +0900

    tutrial

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ for (const folder of commandFolders) {
 }
 
 client.once(Events.ClientReady, () => {
-	console.log('Ready!');
+	console.log(`Ready! Logged in as ${app.client.user}`);
 });
 
 client.on(Events.InteractionCreate, async interaction => {
