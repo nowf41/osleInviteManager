d2a2eb8d1f486c9f50822afef4432bee0937109e
nowf41 <wheat.komugi41@gmail.com>
2023-10-19 01:07:36 +0900

    tutrial

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -1,7 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('ping')
+    data: new SlashCommandBuilder()
+        .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(interaction){
         await interaction.reply('Pong!');
