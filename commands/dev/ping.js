275e05d60284a6c7841ff31323361829853e9e9e
nowf41 <wheat.komugi41@gmail.com>
2023-10-24 18:03:34 +0900

    micro fix

--- commands/dev/ping.js
+++ commands/dev/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Pong!'),
+        .setDescription('Replies with Pong and your username!'),
     async execute(interaction){
         const reply = `Pong! Got ping successfully from ${interaction.user.username}`
         await interaction.reply({content: reply, ephemeral: true });
