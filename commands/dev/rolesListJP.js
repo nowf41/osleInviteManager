028ffe69a48247dc7b5427ecb5f500133319d50d
nowf41 <wheat.komugi41@gmail.com>
2023-10-25 00:07:00 +0900

    ロール一覧に各ロールの保持者数を表示するように設定しました。

--- commands/dev/rolesListJP.js
+++ commands/dev/rolesListJP.js
@@ -6,7 +6,7 @@ module.exports = {
         .setDescription('このサーバーでのロールの一覧を表示します。'),
     async execute(interaction){
         let replyText = '```';
-        interaction.guild.roles.cache.forEach(role => replyText += `${role.name}\n`);
+        interaction.guild.roles.cache.forEach(role => replyText += `${role.name} (${role.members.size} 人のメンバー)\n`);
         replyText += '```';
         await interaction.reply({content: replyText, ephemeral: true });
     }
