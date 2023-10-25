1edbc42d87c6ce3ca66ce018cfbe68948dc807e2
nowf41 <wheat.komugi41@gmail.com>
2023-10-26 00:36:22 +0900

    fixできなかったorz

--- commands/dev/joinReq.js
+++ commands/dev/joinReq.js
@@ -10,10 +10,16 @@ module.exports = {
             .setDescription('Set serverID you want to join')
             .setRequired(true)),
         async execute(interaction) {
-            await interaction.reply({content: 'sending...', ephemeral: true});
             let elem;
+            const user = interaction.user;
+            const guild = interaction.client.guilds.cache.get(interaction.options.getString('serverid'))
+            const channel = guild.channels.cache.forEach(element => {if(element.name === 'joinrequest'){elem = element}});
+
+            await interaction.reply({content: 'sending...', ephemeral: true});
             if(interaction.options.getString('serverid').length < 18) return
-            const channel = interaction.client.guilds.cache.get(interaction.options.getString('serverid')).channels.cache.forEach(element => {if(element.name === 'joinrequest'){elem = element}});
+            guild.members.cache.forEach(element =>{
+                    if(element === user){interaction.editReply('Cancelled request: You are already belongs to this server.')};console.log(element)});
+
             elem.send(`Got new join request: ${interaction.user.toString()}`)
                 .then(interaction.editReply({content: `Success! sent a request.`, ephemeral: true}))
                 .catch((e) => interaction.editReply({content: `An error has occured, your request didn't sent: ${e}`, ephemeral: true}))
